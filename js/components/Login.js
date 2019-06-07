fd7386ee3fd005367a71ac5c59461dc5d6f06364
Vanya Kohut <i.kohut@yahoo.com>
2019-06-07 12:59:59 +0200

    added login and register pages with styling

--- js/components/Login.js
+++ js/components/Login.js
@@ -1,9 +1,85 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+import styled from 'styled-components';
+
+const LoginContainer = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+`;
+
+const LoginForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    height: 70%;
+    width: 60%;
+`;
+
+const LoginHeader = styled.h2`
+    text-align: center;
+    font-size: 2rem;
+    &:after {
+        content: url('../../images/decoration.png');
+        display: block;
+        width: 100%;
+        transform: scale(.7);
+        margin-top: 10px;
+    }
+`;
+
+const EmailInput = styled.input`
+    outline: none;
+    border: none;
+    border-bottom: 1px solid #696969;
+    padding: 10px;
+    font-size: 1.1rem;
+    font-weight: 300;`;
+
+const PasswordInput = styled.input`
+    outline: none;
+    border: none;
+    border-bottom: 1px solid #696969;
+    padding: 10px;
+    font-size: 1.1rem;
+    font-weight: 300;`;
+
+const LoginLinks = styled.section`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const RegisterLink = styled(Link)`
+    text-decoration: none;
+    color: #696969;
+    padding: 10px;
+`;
+
+const LogIn = styled.input`
+    outline: none;
+    border: 1px solid #696969;
+    padding: 10px;
+    font-size: 1rem;
+    font-weight: 300;
+`;
 
 class Login extends Component {
     render() {
         return (
-            <h3>Log in</h3>
+            <LoginContainer>
+                <LoginForm>
+                    <LoginHeader>Zaloguj się</LoginHeader>
+                    <EmailInput type='email' placeholder='Email'/>
+                    <PasswordInput type='password' placeholder='Hasło'/>
+
+                    <LoginLinks>
+                        <RegisterLink to='register'>Załóz konto</RegisterLink>
+                        <LogIn type='submit' value='Zaloguj się'/>
+                    </LoginLinks>
+                </LoginForm>
+            </LoginContainer>
         );
     }
 }
