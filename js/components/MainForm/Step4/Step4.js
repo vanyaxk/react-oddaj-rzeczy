08cfbe9813febb8d238acdbe6cc2fb121915c98b
Vanya Kohut <i.kohut@yahoo.com>
2019-06-20 13:07:35 +0200

    added markup to step 4

--- js/components/MainForm/Step4/Step4.js
+++ js/components/MainForm/Step4/Step4.js
@@ -1,16 +1,44 @@
 import React, {Component} from 'react';
 
 import {
-    FormStepFour
+    FormStepFour,
+    StepFourHeader,
+    AddressHeader,
+    AddressInputs,
+    AddressLabel,
+    DeadlineInputs,
+    AddressInput,
+    DeadlineHeader,
+    DeadlineInput,
+    DeadlineMessage,
+    DeadlineLabel
 } from './Step4Styles'
 
 class Step4 extends Component {
     render() {
         if (this.props.formStep !== 4) return null;
         return (
+            <>
+            <StepFourHeader>Podaj adres oraz termin odbioru rzeczy przez kuriera</StepFourHeader>
             <FormStepFour>
-                <StepFourHeader>Podaj adres oraz termin odbioru rzeczy przez kuriera</StepFourHeader>
+                
+                <AddressInputs>
+                <AddressHeader>Adres odbioru</AddressHeader>
+                    <AddressLabel>Ulica <AddressInput /></AddressLabel>
+                    <AddressLabel>Miasto <AddressInput /></AddressLabel>
+                    <AddressLabel>Kod Pocztowy <AddressInput /></AddressLabel>
+                    <AddressLabel>Numer telefonu <AddressInput /></AddressLabel>
+                </AddressInputs>
+
+                
+                <DeadlineInputs>    
+                <DeadlineHeader>Termin odbioru</DeadlineHeader>
+                    <DeadlineLabel>Data <DeadlineInput /></DeadlineLabel>
+                    <DeadlineLabel>Godzina <DeadlineInput /></DeadlineLabel>
+                    <DeadlineLabel>Uwagi dla kuriera <DeadlineMessage /></DeadlineLabel>
+                </DeadlineInputs>
             </FormStepFour>
+            </>
         )
     }
 }
--- js/components/MainForm/Step4/Step4Styles.js
+++ js/components/MainForm/Step4/Step4Styles.js
@@ -1,5 +1,76 @@
 import styled from 'styled-components';
 
 export const FormStepFour = styled.section`
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const StepFourHeader = styled.h4`
+    text-align: center;
+    font-size: 1rem;
+    font-weight: 700;
+`;
+
+export const AddressLabel = styled.label`
+    margin: 10px 0;
+`;
+
+export const AddressHeader = styled.h5`
+    font-size: .7rem;
+    font-weight: 700;
+`;
+
+export const AddressInputs = styled.section`
+    display: flex;
+    flex-direction: column;
+    width: 45%;
     
+`;
+
+export const AddressInput = styled.input.attrs({type: 'text'})`
+    display: block;
+    width: 100%;
+    margin-top: 5px;
+    padding: 10px;
+    font-size: 1rem;
+    font-weight: 300;
+`;
+
+
+export const DeadlineLabel = styled.label`
+    margin: 10px 0;
+`;
+
+export const DeadlineHeader = styled.h5`
+    font-size: .7rem;
+    font-weight: 700;
+    `;
+
+export const DeadlineInputs = styled.section`
+    display: flex;
+    flex-direction: column;
+    width: 45%;
+`;
+
+
+export const  DeadlineInput = styled.input.attrs({type: 'text'})`
+    display: block;
+    width: 100%;
+    margin-top: 5px;
+    padding: 10px;
+    font-size: 1rem;
+    font-weight: 300;
+`;
+
+export const DeadlineMessage = styled.textarea.attrs({wrap: 'hard', rows: '5'})`
+    resize: none;
+    margin-top: 10px;
+    padding: 10px;
+    width: 100%;
+    font-size: 1rem;
+    font-weight: 300;
+    border: 1px solid grey;
+    &:focus {
+        outline: none;
+    }
 `;
